1f4d4d3ecb5240fc5688f19df7223680732ac930
Grillo200 <100572222+Grillo200@users.noreply.github.com>
2022-04-28 08:52:48 +0200

    algo jose

--- src/Outputs/TerminalOutput.cs
+++ src/Outputs/TerminalOutput.cs
@@ -17,6 +17,7 @@ namespace DocumentPlagiarismChecker.Outputs
     /// This output base object sends the results to the terminal.
     /// </summary>
     internal class TerminalOutput: Core.BaseOutput{
+    /// Comentario de jose chistoso wow
     /// Bua comentario de pau increible
         public TerminalOutput(): base(){
         }
